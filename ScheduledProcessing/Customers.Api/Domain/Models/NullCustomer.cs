10410b1072b90d6d4bc82ee822ba3ab3176a1910
icarotorres <icaro.stuart@gmail.com>
2021-03-18 13:37:49 -0300

    chore(domain-models): remove internal access modifiers for serialization for the sake of possible serializations.

--- ScheduledProcessing/Customers.Api/Domain/Models/NullCustomer.cs
+++ ScheduledProcessing/Customers.Api/Domain/Models/NullCustomer.cs
@@ -4,8 +4,8 @@ namespace Customers.Api.Domain.Models
 {
     public class NullCustomer : Customer, INull
     {
-        public override ulong Cpf { get => 0; internal set { } }
-        public override string Name { get => string.Empty; internal set { } }
-        public override string State { get => string.Empty; internal set { } }
+        public override ulong Cpf { get => 0; set { } }
+        public override string Name { get => string.Empty; set { } }
+        public override string State { get => string.Empty; set { } }
     }
 }
