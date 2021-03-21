c66bdf8a9bb7fa7b26bbd8671f624d506808c4d7
Icaro Torres <icaro.stuart@gmail.com>
2021-03-21 02:28:54 -0300

    tests(processing-worker): add initial structures for processing worker tests.

---
+++ ScheduledProcessing/Processing.Worker.UnitTests/Domain/Services/AmountProcessorTests.cs
@@ -0,0 +1,39 @@
+﻿using Bogus;
+using FluentAssertions;
+using Library.TestHelpers;
+using Processing.Worker.Domain.Models;
+using Processing.Worker.Domain.Services;
+using Xunit;
+
+namespace Processing.Worker.UnitTests.Domain.Services
+{
+    [Trait("unit-test", "processing.worker-domain")]
+    public class AmountProcessorTests
+    {
+        [Fact]
+        public void MathOnlyAmountProcessor_Calculate()
+        {
+            // arrange
+            var cpf = Fakes.CPFs.Valid.Generate();
+            var str = cpf.ToString();
+            var customer = new Customer { Cpf = cpf };
+            var billing = new Billing { Cpf = cpf, Amount = new Faker().Random.Decimal(1, 1000) };
+            var processedAtValueBeforeProcess = billing.ProcessedAt;
+            var digits = new char[4] { str[0], str[1], str[^2], str[^1] };
+            var expectedAmount = decimal.Parse(digits);
+            var sut = new MathOnlyAmountProcessor();
+
+            // act
+            var result = sut.Process(customer, billing);
+
+            // assert
+            result.Should().NotBeNull()
+                .And.BeOfType<Billing>()
+                .And.BeSameAs(billing);
+            result.Amount.Should().Be(expectedAmount);
+            result.Amount.ToString("0000").Should().Be(string.Join("", digits));
+            result.ProcessedAt.Should().NotBeNull();
+            processedAtValueBeforeProcess.Should().BeNull();
+        }
+    }
+}
---
+++ ScheduledProcessing/Processing.Worker.UnitTests/Domain/Services/CustomerCpfComparerTests.cs
@@ -0,0 +1,82 @@
+﻿using Bogus;
+using FluentAssertions;
+using Library.TestHelpers;
+using Processing.Worker.Domain.Models;
+using Processing.Worker.Domain.Services;
+using Xunit;
+
+namespace Processing.Worker.UnitTests.Domain.Services
+{
+    [Trait("unit-test", "processing.worker-domain")]
+    public class CustomerCpfComparerTests
+    {
+        [Fact]
+        public void Sort_Should_Sort_Collection_Correctly()
+        {
+            // arrange
+            var collectionToSort = Fakes.CPFs.Valid.Generate(3).ConvertAll(x => new Customer { Cpf = x });
+            var sut = new CustomerCpfComparer();
+
+            // act
+            collectionToSort.Sort(sut);
+
+            // assert
+            collectionToSort.Should().BeInAscendingOrder(sut);
+            ulong aux = 0;
+            collectionToSort.TrueForAll(x =>
+            {
+                var isGreater = x.Cpf > aux;
+                aux = x.Cpf;
+                return isGreater;
+            }).Should().BeTrue();
+        }
+
+        [Fact]
+        public void BinarySearch_Should_Return_CollectionItem_Index_Correctly()
+        {
+            // arrange
+            var collectionToSort = Fakes.CPFs.Valid.Generate(3).ConvertAll(x => new Customer { Cpf = x });
+            var index = new Faker().Random.Int(0, collectionToSort.Count - 1);
+            var sut = new CustomerCpfComparer();
+            collectionToSort.Sort(sut);
+
+            // act
+            var result = collectionToSort.BinarySearch(collectionToSort[index], sut);
+
+            // assert
+            collectionToSort.Should().BeInAscendingOrder(sut);
+            ulong aux = 0;
+            collectionToSort.TrueForAll(x =>
+            {
+                var isGreater = x.Cpf > aux;
+                aux = x.Cpf;
+                return isGreater;
+            }).Should().BeTrue();
+            result.Should().Be(index);
+        }
+
+        [Fact]
+        public void BinarySearch_Should_Return_NoCollectionItem_NegativeIndex()
+        {
+            // arrange
+            var collectionToSort = Fakes.CPFs.Valid.Generate(3).ConvertAll(x => new Customer { Cpf = x });
+            var notInCollection = new Customer { Cpf = Fakes.CPFs.Valid.Generate() };
+            var sut = new CustomerCpfComparer();
+            collectionToSort.Sort(sut);
+
+            // act
+            var result = collectionToSort.BinarySearch(notInCollection, sut);
+
+            // assert
+            collectionToSort.Should().BeInAscendingOrder(sut);
+            ulong aux = 0;
+            collectionToSort.TrueForAll(x =>
+            {
+                var isGreater = x.Cpf > aux;
+                aux = x.Cpf;
+                return isGreater;
+            }).Should().BeTrue();
+            result.Should().BeLessThan(0);
+        }
+    }
+}
