da0e97c9d45ea50be41dda75adb1944aaa6e943e
Icaro Torres <icaro.stuart@gmail.com>
2021-03-25 12:09:06 -0300

    fix: benchmark namespace references.

--- Processing.EventualBenchmarks/AmountProcessorBenchmarks.cs
+++ Processing.EventualBenchmarks/AmountProcessorBenchmarks.cs
@@ -1,7 +1,5 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Order;
-using Processing.Worker.Application.Services;
-using Processing.Worker.Domain.Models;
 
 namespace Processing.Benchmarks
 {
@@ -10,14 +8,31 @@ namespace Processing.Benchmarks
     [RankColumn]
     public class AmountProcessorBenchmarks
     {
-        private static readonly Customer _customer = new Customer { Cpf = 92903084530 };
-        private static Billing _billing = new Billing { Cpf = 92903084530 };
-        private static readonly MathOnlyAmountProcessor _mathOnlyProcessor = new MathOnlyAmountProcessor();
+        private static readonly ScheduledWorker.Domain.Models.Customer _customer =
+            new ScheduledWorker.Domain.Models.Customer { Cpf = 92903084530 };
+        private static ScheduledWorker.Domain.Models.Billing _billing =
+            new ScheduledWorker.Domain.Models.Billing { Cpf = 92903084530 };
+        private static readonly ScheduledWorker.Domain.Services.MathOnlyAmountProcessor _mathOnlyProcessor =
+            new ScheduledWorker.Domain.Services.MathOnlyAmountProcessor();
+
+        private static readonly EventualWorker.Domain.Models.Customer _customer2 =
+            new EventualWorker.Domain.Models.Customer { Cpf = 92903084530 };
+        private static EventualWorker.Domain.Models.Billing _billing2 =
+            new EventualWorker.Domain.Models.Billing { Cpf = 92903084530 };
+        private static readonly EventualWorker.Application.Services.MathOnlyAmountProcessor _mathOnlyProcessor2 =
+            new EventualWorker.Application.Services.MathOnlyAmountProcessor();
+
 
         [Benchmark]
-        public void MathOnlyProcessor_Process()
+        public void Scheduled_MathOnlyProcessor_Process()
         {
             _billing = _mathOnlyProcessor.Process(_customer, _billing);
+        }
+
+        [Benchmark]
+        public void Eventual_MathOnlyProcessor_Process()
+        {
+            _billing2 = _mathOnlyProcessor2.Process(_customer2, _billing2);
         }
     }
 }
